a8d39c17d85f9b924cb230e4d6c76a8be72db2e3
David Bernard <david.bernard.31@gmail.com>
2014-05-22 17:31:53 +0200

    lemur_ext: create helpers for Model

--- src/main/java/lemur_ext/Widgets.java
+++ src/main/java/lemur_ext/Widgets.java
@@ -3,8 +3,14 @@ package lemur_ext;
 
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.control.AbstractControl;
 import com.simsilica.lemur.Container;
+import com.simsilica.lemur.Label;
 import com.simsilica.lemur.component.TbtQuadBackgroundComponent;
+import com.simsilica.lemur.core.VersionedObject;
+import com.simsilica.lemur.core.VersionedReference;
 
 /**
  *
@@ -24,5 +30,24 @@ public class Widgets {
         r.setLayout(layout);
         r.setPreferredSize(new Vector3f(camera.getWidth(), camera.getHeight(), 0));
         return r;
+    }
+    
+    public static <T> void bindLabel(final Label label, VersionedObject<T> o, boolean autoUpdate) {
+    	final VersionedReference<T> ref = o.createReference();
+    	label.setText(ref.get().toString());
+    	if (autoUpdate) {
+	    	label.addControl(new AbstractControl() {
+				@Override
+				protected void controlRender(RenderManager arg0, ViewPort arg1) {
+				}
+	
+				@Override
+				protected void controlUpdate(float arg0) {
+					if (ref.update()) {
+						label.setText(ref.get().toString());
+					}
+				}
+			});
+    	}
     }
 }
